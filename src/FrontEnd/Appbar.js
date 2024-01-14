7d716adaff13dc4392216702b4819a2def77afcd
Anuj3366 <anujgarg3366@gmail.com>
2024-01-15 00:37:06 +0530

    added material UI , added basic routes

---
+++ src/FrontEnd/Appbar.js
@@ -0,0 +1,7 @@
+export default function Appbar() {
+    return (
+        <div>
+            <h1>Appbar</h1>
+        </div>
+    )
+}
---
+++ src/FrontEnd/AuctionStatus.js
@@ -0,0 +1,7 @@
+export default function AuctionStatus() {
+  return (
+    <div>
+      <h1>AuctionStatus</h1>
+    </div>
+  )
+} 
