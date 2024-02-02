18f5ef06641e7203bc1ce42047b3ba699e3d4b6b
Anuj3366 <anujgarg3366@gmail.com>
2024-02-02 19:31:58 +0530

    reinstalled

--- src/FrontEnd/Appbar.js
+++ src/FrontEnd/Appbar.js
@@ -2,6 +2,10 @@ export default function Appbar() {
     return (
         <div>
             <h1>Appbar</h1>
+            {/* <Link href="/" component={Homepage} />
+            <Link href="/login" component={Login} />
+            <Link href="/signup" component={Signup} />
+            <Link href="/auctionstatus" component={AuctionStatus} /> */}
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import Homepage from '../FrontEnd/Homepage'
-import Login from '../FrontEnd/Login'
-import Signup from '../FrontEnd/Signup'
-import Appbar from '../FrontEnd/Appbar'
-import AuctionStatus from '../FrontEnd/AuctionStatus'
 export default function Home() {
   return (
     <div>
       <Appbar />
-      <Link path="/" component={Homepage} />
-      <Link path="/login" component={Login} />
-      <Link path="/signup" component={Signup} />
-      <Link path="/auctionstatus" component={AuctionStatus} />
     </div>
   )
 }
