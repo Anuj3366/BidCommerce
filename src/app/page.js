d5798359733db3a2f97495da487808d6afe57781
Anuj3366 <anujgarg3366@gmail.com>
2024-02-02 15:26:21 +0530

    used link to link pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from '../FrontEnd/Homepage'
 import Login from '../FrontEnd/Login'
 import Signup from '../FrontEnd/Signup'
 import Appbar from '../FrontEnd/Appbar'
 import AuctionStatus from '../FrontEnd/AuctionStatus'
-// import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 export default function Home() {
   return (
     <div>
-      {/* <AppRouterCacheProvider>{children}</AppRouterCacheProvider> */}
-      <BrowserRouter>
-        <Routes>
-          <Appbar />
-          <Route path="/" component={Homepage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/auctionstatus" component={AuctionStatus} />
-        </Routes>
-      </BrowserRouter>
+      <Appbar />
+      <Link path="/" component={Homepage} />
+      <Link path="/login" component={Login} />
+      <Link path="/signup" component={Signup} />
+      <Link path="/auctionstatus" component={AuctionStatus} />
     </div>
   )
 }
