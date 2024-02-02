d391948118d2f083014e3ee1159771afe454061f
Anuj3366 <anujgarg3366@gmail.com>
2024-02-02 15:22:51 +0530

    Started Project

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Route } from 'react-router'
-import { BrowserRouter , Router,} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from '../FrontEnd/Homepage'
 import Login from '../FrontEnd/Login'
 import Signup from '../FrontEnd/Signup'
 import Appbar from '../FrontEnd/Appbar'
 import AuctionStatus from '../FrontEnd/AuctionStatus'
+// import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 export default function Home() {
   return (
     <div>
+      {/* <AppRouterCacheProvider>{children}</AppRouterCacheProvider> */}
       <BrowserRouter>
-        <Router>
+        <Routes>
           <Appbar />
           <Route path="/" component={Homepage} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
           <Route path="/auctionstatus" component={AuctionStatus} />
-        </Router>
+        </Routes>
       </BrowserRouter>
     </div>
   )
