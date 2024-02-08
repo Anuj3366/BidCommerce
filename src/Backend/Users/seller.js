22bb24377a9e2f5fa39c9c0a6667dfbe2e9ae330
Anuj3366 <anujgarg3366@gmail.com>
2024-02-09 00:57:45 +0530

    Commited design document

--- src/Backend/Users/seller.js
+++ src/Backend/Users/seller.js
@@ -1,13 +1,3 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
-import connectDB from '../connectDB.js';
-
-const app = express();
-app.use(express.json());
-connectDB();
-
-const secret = 'secrettohide';
 const SellerSchema = new mongoose.Schema({
   shopname: String,
   shopemail: String,
--- src/Backend/Users/worker.js
+++ src/Backend/Users/worker.js
@@ -1,13 +1,3 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
-import connectDB from '../connectDB.js';
-
-const app = express();
-app.use(express.json());
-connectDB();
-
-const secret = 'secrettohide';
 const WorkerSchema = new mongoose.Schema({
   name: String,
   email: String,
