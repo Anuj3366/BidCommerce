d045138cd7f6513f0bfe04d0553384a033d1fde3
Anuj3366 <anujgarg3366@gmail.com>
2024-02-14 12:31:56 +0530

    Add compiler option for styled components and format Center component

--- src/components/Center.js
+++ src/components/Center.js
@@ -6,8 +6,10 @@ const StyledDiv = styled.div`
   padding: 0 20px;
 `;
 
-export default function Center({children}) {
+export default function Center({ children }) {
   return (
-    <StyledDiv>{children}</StyledDiv>
+    <StyledDiv>
+      {children}
+    </StyledDiv>
   );
 }
