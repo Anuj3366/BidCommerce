f5f0ea6275df984f2a241bb825c3273c5016574b
Anuj3366 <anujgarg3366@gmail.com>
2024-03-18 23:40:29 +0530

    Refactor middleware setup and add static file serving

--- Backend/index.js
+++ Backend/index.js
@@ -4,42 +4,47 @@ const cors = require('cors');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+
+// Middleware setup
 app.use(cookieParser());
-app.use(cors({origin: 'http://localhost:1234', credentials: true}));
+app.use(cors({ origin: 'http://localhost:1234', credentials: true }));
 app.use(bodyParser.json());
-app.use('/', router);
+
+// Connect to MongoDB (assuming you have a separate file for this)
 const connectDB = require('./mongoDB');
 connectDB();
 
+// Import your API routes
 const getAllItems = require('./api/getAll');
 app.use('/', getAllItems);
-console.log("1");
+
 const cart = require('./api/cart');
 app.use('/', cart);
-console.log("2");
+
 const checkout = require('./api/checkout');
 app.use('/', checkout);
-console.log("3");
+
 const login = require('./api/login');
 app.use('/', login);
-console.log("4");
+
 const postAll = require('./api/postAll');
 app.use('/', postAll);
-console.log("5");
+
 const update = require('./api/update');
 app.use('/', update);
-console.log("6");
+
 const webhook = require('./api/webhook');
 app.use('/', webhook);
-console.log("7");
+
 const account = require('./api/account');
 app.use('/', account);
-console.log("8");
 
 console.log("All routes are running");
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-
+// Start the server
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
