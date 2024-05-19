47984b13f40b7960b7f8d8c8f1172b9ba6b7bade
Anuj3366 <anujgarg3366@gmail.com>
2024-05-19 13:18:59 +0530

    chore: Update MongoDB connection to use environment variable

--- Backend/mongoDB.js
+++ Backend/mongoDB.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://anujgarg3366:Lipika%40987@cluster.lqk4zfl.mongodb.net/');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error on Connecting MongoDB: ${error.message}`);
